dca0e0f341fd14616faf7c67829ee215b45eef2a
phsu40 <phsu40@gatech.edu>
2023-05-01 20:15:46 -0700

    Uplift user data states through App and then pass to Users List component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
+const DUMMY_USERS = [
+  {username: 'Jona', age: 27, id: Math.random().toString()},
+  {username: 'Laura', age: 25, id: Math.random().toString()}
+]
 
 function App() {
+
+  const [usersList, setUsersList] = useState(DUMMY_USERS);
+
+  const addUserHandler = (username, age) => {
+    setUsersList((prev) => {
+      return [...prev, {username: username, age: age, id: Math.random().toString()}];
+    })
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList}/>
     </div>
+
   );
 }
 
