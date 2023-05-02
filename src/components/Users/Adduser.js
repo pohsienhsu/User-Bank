70f2395606d6a62a403a3ceb0e8224746fa138da
phsu40 <phsu40@gatech.edu>
2023-05-01 19:25:30 -0700

    Integrate change handlers to add user form inputs

--- src/components/Users/Adduser.js
+++ src/components/Users/Adduser.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../UI/Card';
 
 import styles from './AddUser.module.css';
+import Button from '../UI/Button';
 
 const AddUser = (props) => {
 
+  const [inputUsername, setInputUsername] = useState('');
+  const [inputAge, setInputAge] = useState('');
+
   const addUserHandler = (event) => {
-    event.prevenDefault();
-    console.log('worked');
+    event.preventDefault();
+    console.log(inputUsername, inputAge);
+    setInputUsername('');
+    setInputAge('');
   }
+
+  const usernameChangeHandler = (event) => {
+    setInputUsername(event.target.value);
+  }
+
+  const ageChangeHandler = (event) => {
+    setInputAge(event.target.value);
+  } 
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username" >Username</label>
-        <input id='username' type='text' />
+        <input id='username' type='text' value={inputUsername} onChange={usernameChangeHandler} />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' />
-        <button type='submit'>Add User</button>
+        <input id='age' type='number' value={inputAge} onChange={ageChangeHandler} />
+        <Button type='submit'>Add User</Button>
       </form>
     </Card>
   )
