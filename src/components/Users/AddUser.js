b6a22026ff72ecb2efde30640ea151f207408cdb
phsu40 <phsu40@gatech.edu>
2023-05-02 20:05:12 -0700

    Create wrapper component to avoid redundant nested div

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
 
@@ -50,7 +51,7 @@ const AddUser = (props) => {
   }
 
   return (
-    <div>
+    <Wrapper>
       {error && <ErrorModal title={error.title} message={error.message} onDismiss={dismissErrorHandler} />}
       <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
@@ -71,7 +72,7 @@ const AddUser = (props) => {
           <Button type='submit'>Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   )
 }
 
